7a5d8484617f228ac80221ea7eaf6ca29cf650e7
amcgamer <albert.montagut.casero@gmail.com>
2017-10-23 15:00:46 +0200

    preparing to create AI Controller System

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -5,7 +5,13 @@
 void ATankPlayerController::BeginPlay() 
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Playercontroller not possesing a tank"));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Playercontroller possesing: %s"), *(ControlledTank->GetName()));
 }
 
 ATank* ATankPlayerController::GetControlledTank() const 
